fee54c35c2f3397656a13e74fb000dc89f15c2b4
HeJingjing <campanulamediuml@gmail.com>
2018-03-28 10:58:19 +0800

    假装自己在写代码

--- common_unit/common_unit.go
+++ common_unit/common_unit.go
@@ -93,9 +93,9 @@ func Write_log(content string) error {
     return err
 }
 
-func Get_token(content string) error{
-    return err
-}
+// func Get_token(content string) error{
+//     return err
+// }
 
 // func Add_token(content string,username string,password string)(result_content string){
 //     var content_head string= `
